57c98d267f2bf2050c1b53170e0afcb09c2d60e7
Shakrah Yves <7shakrahs@gmail.com>
2019-07-17 14:09:50 +0200

    update fluture

--- src/routes.js
+++ src/routes.js
@@ -6,11 +6,16 @@ const knex = require ('knex') (require('../knexfile'));
 const {Literal, Wild} = require ('./Component');
 const JsonResponse = require ('./JsonResponse');
 const Response = require ('./Response');
-const db = require ('./db');
 const S = require ('./sanctuary');
-const pg = require ('../store');
 
 module.exports = [
+
+  //when one makes a list, one can update it many times.
+  //One can add individual ingredients.
+  //one can add multiple ingredients.
+  //one can add ingrediants from a recipe.see recipe.js
+  //Q: could reipes also have a feature so that ingredients
+  //could be automatically added to an existing list?
 
   S.Pair ([Literal ('recipes')]) ({
     GET: captures => {
@@ -20,7 +25,19 @@ module.exports = [
       );
       return S.map (JsonResponse.OK ({})) (f (captures));
    },
+    //POST: captures =>
+    //const f = Future.encaseP (captures =>
+      //knex('recipe').insert({captures.name}, ['id', 'name'])
+    //);
+      //return S.map (JsonResponse.OK ({})) (f (captures));
   }),
+
+  //TODO get, update, delete by ID.
+  //S.Pair ([Literal ('recipes')]) ({
+   // GET
+    //UPDATE ...remove replace an ingredient, amount, or description.*
+    //DELETE
+  //}),
 
   S.Pair ([Literal ('ingredients')]) ({
     GET: captures => {
@@ -30,18 +47,17 @@ module.exports = [
             .from ('ingredient')
       );
       return S.map (JsonResponse.OK ({})) (f (captures));
-    },
+     },
 
-    POST: captures => {
-      const f = Future.encaseP (captures =>
-        knex('ingredient').insert(captures.name, ['id', 'name'])
-      );
-      return S.chain (S.array (Future.reject (Response.InternalServerError ({}) ('')))
-                              (head => tail => Future.of (JsonResponse.Ok ({}) (head))))
-                              (f (captures));
-
-    // return Future.of (S.maybe (knex('ingredient').insert({captures.name})
-    },
+   POST: captures => {
+    const f = Future.encaseP (captures =>
+      knex('ingredients').insert({name: captures.name})
+    );
+    //return S.chain (S.array (Future.reject (Response.InternalServerError ({}) ('')))
+      //                      (head => tail => Future.of (JsonResponse.Ok ({}) (head))))
+        //                    (f (captures));
+      return S.map (JsonResponse.OK ({})) (f (captures));
+    }
   }),
 
   S.Pair ([Literal ('ingredients'), Wild ('id')]) ({
@@ -56,12 +72,18 @@ module.exports = [
                               (head => tail => Future.of (JsonResponse.OK ({}) (head))))
                      (f (captures));
     },
-    DELETE: captures =>
-      Future.of (S.maybe (Response.NotFound ({}) (''))
-                         (ingredient => (delete db.ingredients[String (ingredient.id)],
-                                         Response.NoContent ({}) ('')))
-                         (S.value (captures.id) (db.ingredients))),
-
+    DELETE: captures => {
+      const f = Future.encaseP (captures =>
+        knex.select ('id', 'name')
+            .from('ingredient')
+            .where ('id', '=', captures.id)
+      );
+      return S.chain (S.array (Future.reject (Response.NotFound ({}) ('')))
+                              (head => tail => Future.of (Repsonse.NoContent ({}) ('')))
+                              (f (captures)));
+    }
   }),
 
 ];
+
+
