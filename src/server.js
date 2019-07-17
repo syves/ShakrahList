7c91e60e171a75efbeb4c6de6eaf65f5eb4fc3ab
Shakrah Yves <7shakrahs@gmail.com>
2019-07-17 17:37:01 +0200

    try to debug matches

--- src/server.js
+++ src/server.js
@@ -17,22 +17,31 @@ const maybeToFuture = x => maybe =>
           /* return type */    // #4     b          Future x y
 
 const server = http.createServer ((req, res) => {
+      console.log('reqUrl: '+req.url+ ' resMeth '+ req.method + ' reqData '+ res.body);
   //    future :: Future Error Response
   const future =
+  //reduce handlers to a single matching route, by folding into a Future.
   S.reduce (future => ([desc, handlers]) =>
+              // todo remove
+              //console.log('desc ' + desc);
+              S.map (m => console.log(S.value (m))) (matches (desc) (req.url));
+              //We use alt to choose between two futures?
+              //chain is flatMap which feeding 'captures':: Future Error StrMap
+              // to S.maybe (see 34), only if the value is a Maybe,
               S.alt (S.chain (captures => {
-                                const Allow =
+                                const AllowedMethods =
                                   S.joinWith (', ') (Object.keys (handlers));
                                 const defaultResponse =
-                                  Response.MethodNotAllowed ({Allow}) ('');
-                                //:: Maybe Future Response
+                                  Response.MethodNotAllowed ({AllowedMethods}) ('');
+                                //if the req.Method is suported, then pass the-
+                                //the captures :: StrMap to the functon,
+                                //or yield error for meth not allowed.
                                 return S.maybe (Future.of (defaultResponse))
                                                (handler => handler (captures))
-                                               // :: ? request method from handers gives
-                                                // functions?
                                                (S.value (req.method)
                                                         (handlers));
                               })
+                              //:: Future (Err) (Maybe StrMap)
                              (maybeToFuture ('Path did not match')
                                             (matches (desc) (req.url))))
                     (future))
