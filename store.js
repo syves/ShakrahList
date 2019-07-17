cea9bb75329fba0dc1adde8d2bb58a54b7827340
Shakrah Yves <7shakrahs@gmail.com>
2019-07-17 14:12:23 +0200

    remove unused file

--- store.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const knex = require ('knex') (require ('./knexfile'));
-
-exports.createIngredient = ({name}) => {
-  console.log (`Add ingredient ${name}`);
-  return knex ('ingredient')
-         .insert ({name});
-};
