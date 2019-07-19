d4dd1f41a544d1cbec4a7f5fd3c3d8dc1fab0e18
Shakrah Yves <7shakrahs@gmail.com>
2019-07-19 18:40:50 +0200

    can delete, wip update

--- src/routes.js
+++ src/routes.js
@@ -22,6 +22,7 @@ module.exports = [
   }),
 
   S.Pair ([Literal ('ingredients')]) ({
+    // READ ingredients
     GET: captures => body => {
       //    f :: StrMap String -> Future Error (Array Ingredient)
       const f = Future.encaseP (captures =>
@@ -30,15 +31,7 @@ module.exports = [
       );
       return S.map (JsonResponse.OK ({})) (f (captures));
     },
-/*
-    POST: captures => bodyM =>
-      S.map (JsonResponse.OK ({}))
-            (S.chain (Future.encaseP (body => knex('ingredients').insert({name: body.param1})))
-                     (S.maybe (Future.reject ('Invalid request body'))
-                              (Future.of)
-                              (bodyM))),
-  }),
-  */
+    // CREATE ingredient
     POST: captures => bodyM => {
       //    bodyF :: Future String Body
       const bodyF =
@@ -49,11 +42,11 @@ module.exports = [
       //    insertF :: Body -> Future String Result
       const insertF = body => Future ((reject, resolve) => {
         knex('ingredient')
+        .returning(['id', 'name'])
         .insert({name: body.param1})
-        .then(result => { console.log ('succeeded'); resolve (result); })
+        .then(result => { console.log ('succeeded', result); resolve (result); })
         .error(err => { console.log ('failed'); reject (err); });
       });
-
       return S.map (JsonResponse.OK ({})) (S.chain (insertF) (bodyF));
     }
   }),
@@ -61,24 +54,45 @@ module.exports = [
   S.Pair ([Literal ('ingredients'), Wild ('id')]) ({
     GET: captures => body => {
       //    f :: Future Error (Array Ingredient)
-      const f = Future.encaseP (captures =>
-        knex.select ('id', 'name')
-            .from ('ingredient')
+      const getByIdF = captures => Future ((reject, resolve) => {
+        knex('ingredient')
+            .select ('id', 'name')
             .where ('id', '=', captures.id)
-      );
-      return S.chain (S.array (Future.reject (Response.NotFound ({}) ('')))
-                              (head => tail => Future.of (JsonResponse.OK ({}) (head))))
-                     (f (captures));
+            .then(result => { console.log ('succeeded', result); resolve (result); })
+            .error(err => { console.log ('failed'); reject (err); });
+      });
+      // TODO: 404s do not bubble up to client, only in server logging
+       return S.chain (S.array (Future.reject (Response.NotFound ({}) ('')))
+                             (head => tail => Future.of (JsonResponse.OK ({}) (head))))
+                  (getByIdF (captures));
+      //return S.map (JsonResponse.OK ({})) ( getByIdF (captures));
     },
-    DELETE: captures => body => {
-      const f = Future.encaseP (captures =>
-        knex.select ('id', 'name')
-            .from('ingredient')
+
+    PUT: captures => body => {
+      const updateF = captures => Future ((reject, resolve) => {
+        knex('ingredient')
             .where ('id', '=', captures.id)
-      );
-      return S.chain (S.array (Future.reject (Response.NotFound ({}) ('')))
-                              (head => tail => Future.of (Repsonse.NoContent ({}) ('')))
-                              (f (captures)));
+            .update({name: body.param1}, ['id', 'name'])
+            .then(result => { console.log ('succeeded', result); resolve (result); })
+            .error(err => { console.log ('failed'); reject (err); });
+      });
+      return S.map (JsonResponse.OK ({})) (updateF (captures));
+    },
+
+    DELETE: captures => body => {
+      const delF = captures => Future ((reject, resolve) => {
+        knex('ingredient')
+            .where ('id', '=', captures.id)
+            .del()
+            .then(result => { console.log ('succeeded', result); resolve (result); })
+            .error(err => { console.log ('failed'); reject (err); });
+      });
+      //TODO if delete fails returns 0
+      return S.map (JsonResponse.OK ({})) (delF (captures));
+      // TODO: error x.slices is not a function.
+      //return S.chain (S.array (Future.reject (Response.NotFound ({}) ('')))
+      //                        (head => tail => Future.of (Repsonse.NoContent ({}) ('')))
+      //                        (delF (captures)));
     }
   }),
 ];
