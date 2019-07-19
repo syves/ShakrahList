c324889a2e3a3151e33ac6799a3cc90c811f2c32
Shakrah Yves <7shakrahs@gmail.com>
2019-07-19 16:06:48 +0200

    wip post

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const Future = require ('fluture');
-const knex = require ('knex') (require('../knexfile'));
+const knex = require ('knex') (require('../knexfile.js'));
 
 const {Literal, Wild} = require ('./Component');
 const JsonResponse = require ('./JsonResponse');
@@ -30,28 +30,34 @@ module.exports = [
       );
       return S.map (JsonResponse.OK ({})) (f (captures));
     },
-
+/*
     POST: captures => bodyM =>
       S.map (JsonResponse.OK ({}))
             (S.chain (Future.encaseP (body => knex('ingredients').insert({name: body.param1})))
                      (S.maybe (Future.reject ('Invalid request body'))
                               (Future.of)
                               (bodyM))),
-/*
+  }),
+  */
     POST: captures => bodyM => {
       //    bodyF :: Future String Body
       const bodyF =
       S.maybe (Future.reject ('Invalid request body'))
               (Future.of)
               (bodyM);
+
       //    insertF :: Body -> Future String Result
-      const insertF = Future.encaseP (body => knex('ingredients').insert({name: body.param1}));
-      //    resultF :: Future String Result
-      const resultF = S.chain (insertF) (bodyF);
-      return S.map (JsonResponse.OK ({})) (resultF);
+      const insertF = body => Future ((reject, resolve) => {
+        knex('ingredient')
+        .insert({name: body.param1})
+        .then(result => { console.log ('succeeded'); resolve (result); })
+        .error(err => { console.log ('failed'); reject (err); });
+      });
+
+      return S.map (JsonResponse.OK ({})) (S.chain (insertF) (bodyF));
     }
   }),
-*/
+
   S.Pair ([Literal ('ingredients'), Wild ('id')]) ({
     GET: captures => body => {
       //    f :: Future Error (Array Ingredient)
